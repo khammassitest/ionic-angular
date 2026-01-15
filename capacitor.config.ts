84e43abbf77d5999c81cebe3ae3970424ce691cc
Nader <naderkhammassi37@gmail.com>
2026-01-15 11:42:34 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'smart-parking',
+  webDir: 'www'
+};
+
+export default config;
